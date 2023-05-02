0e10ca7c79104abf21c7105172a6811b8f6a520c
Ivarad <radchuck.iwan@yandex.ru>
2023-05-02 22:27:12 +0300

    Selection sorting

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,23 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        selectAlgorithm(); // Вызов функции, с выбором нужного алгоритма
+        SelectAlgorithm(); // Вызов функции, с выбором нужного алгоритма
     }
 
-    private static boolean selectAlgorithm() // Функция выбора алгоритма
+    private static boolean SelectAlgorithm() // Функция выбора алгоритма
     {
         Scanner input = new Scanner(System.in); // Объявляем и инициализируем сканер, необходимый для получения введенных пользователем данных
-        System.out.println("Select algorithm: \n 1. Bubble sort");
+        System.out.println("Select algorithm: \n 1. Bubble sort \n 2. Selection sort");
         if (input.hasNextInt()) {  // Проверяем, является ли введеное значение пользователем, числом
             switch (input.nextInt()) { // Выбираем алгоритм, в зависимости от введеного значения
-                case 1:
-                    new BubbleSort().Sort(); // Вызов сортировки пузырьком
-                    break;
-                case 2:
-                    break;
-
-                default:
+                case 1 -> new BasicSortings().SortBubble(); // Вызов функции сортировки пузырьком
+                case 2 -> new BasicSortings().SortSelection(); // Вызов функции сортировки выбором
+                default -> {
                     return false; // Если пользователь ввел не подходящее значение, завершаем работу функции путем возврата значния
+                }
             }
         } else return false; // Завершаем работу функции, если введеное значние не число
         return true; // Конец функции и возврат значения, при правильно введенных данных
